7462ff4075afa1bd7eef60020b16b215105f64f1
Martin Quintana <m.quintana@buenosaires.gob.ar>
2020-01-14 15:47:30 -0300

    first steps with react + tailwind

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../src/assets/robot.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center container">
+      <header className="bg-purple-900 m-6 p-6 rounded shadow-lg">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-white text-3xl">Star Wars API Challenge</h1>
+      </header>
+      <p className="text-base">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
