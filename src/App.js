61f2c290507ac56b5ed90d32b1acab0ee0cba7e8
Martin Quintana <m.quintana@buenosaires.gob.ar>
2020-01-16 13:05:58 -0300

    change background, build header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from '../src/assets/robot.png';
 import './App.css';
 import { getFilms, ENDPOINT } from './api';
+import { Header } from './components/Header/header';
 
 getFilms(ENDPOINT);
 
 function App() {
   return (
     <div className="text-center container">
+      <Header />
       <header className="bg-purple-900 m-6 p-6 rounded shadow-lg">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="text-white text-3xl">Star Wars API Challenge</h1>
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import swlogo from '../../assets/logo.svg';
+
+export const Header = () => {
+  return (
+    <header className="flex  justify-center">
+      <img
+        src="https://fontmeme.com/permalink/200116/6eb0147ce91ef21bed55cdf6ce4d71ad.png"
+        class=" my-4"
+      />
+    </header>
+  );
+};
