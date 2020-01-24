aa5f46ac1b752eae12355e0296bbe6bb88ea72ae
Martin Quintana <m.quintana@buenosaires.gob.ar>
2020-01-23 21:43:49 -0300

    almost ready

--- src/components/List/film-list.jsx
+++ src/components/List/film-list.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const FilmList = ({ searchResults }) => {
+  const [movies: [], saveMovies] = useState([]);
   const names = searchResults.map(item => item.name);
   const films = searchResults.map(item => item.films);
-
-  console.log(films);
-
+  let allFilms = Array.from(new Set(films.map(JSON.stringify)), JSON.parse);
+  console.log(allFilms);
+  allFilms.map(item => {
+    item.forEach(async item => {
+      let res = await fetch(item);
+      let result = await res.json();
+      let titles = await result.title;
+      return saveMovies(titles);
+    });
+  });
   return (
     <div className="text-yellow-500 font-bold">
       <h2> Personajes elegidos: </h2>
@@ -13,10 +21,8 @@ export const FilmList = ({ searchResults }) => {
         <li>{names[0]}</li>
         <li>{names[1]}</li>
       </ul>
-      <h2>Peliculas en las que participa:</h2>
-      <ul>
-        <li></li>
-      </ul>
+      <h2>Peliculas en las que participan juntos:</h2>
+      <ul>{movies}</ul>
     </div>
   );
 };
