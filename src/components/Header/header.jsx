12fd7b03f96327a6215f93d9f9364db5a7e53de4
Martin Quintana <m.quintana@buenosaires.gob.ar>
2020-01-24 00:10:13 -0300

    fixes

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,6 +6,7 @@ export const Header = () => {
       <img
         src="https://fontmeme.com/permalink/200116/6eb0147ce91ef21bed55cdf6ce4d71ad.png"
         className=" my-4"
+        alt="star wars logo"
       />
     </header>
   );
